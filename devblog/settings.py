419b2bcda796a1cc0b82bbc84313fe26ad7874f0
Matthew Smith <matthewsmithwebdesign@gmail.com>
2020-12-20 16:08:42 -0800

    fixed the injection issues

--- devblog/settings.py
+++ devblog/settings.py
@@ -11,10 +11,15 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+#Templates Directory
+TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -56,7 +61,7 @@ ROOT_URLCONF = 'devblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- polls/urls.py
+++ polls/urls.py
@@ -6,7 +6,6 @@ app_name = 'polls'
 urlpatterns = [
     
      # ex: /polls/
-     
     path('', views.index, name='index'),
     # ex: /polls/5/
     path('specifics/<int:question_id>/', views.detail, name='detail'),
@@ -14,4 +13,5 @@ urlpatterns = [
     path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
+    
 ]
